ccb20f58d95767fd255b6933ca3cf917ba39023e
GvN1989 <geesje.noordenburg@gmail.com>
2024-02-16 13:31:15 +0100

    deel 1 opdracht 1 en 2 v2

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,33 @@ import {calculateProductToBeSold} from "./helpers/calculateProductToBeSold.js";
 import {createProductName} from "./helpers/createProductName.js"
 import {createProductPrice} from "./helpers/createProductPrice.js";
 import {createProductScreenSizeString} from "./helpers/createProductScreenSizeString.js";
+import {createImageTv} from "./helpers/createImageTv.js";
+import minus from './assets/minus.png';
+import check from './assets/check.png';
 
 function App() {
+    function sortBestSellers() {
+        inventory.sort((a, b) => {
+            return a.sold - b.sold
+        });
+        console.log(inventory);
+    }
+
+    function sortCheapest() {
+        inventory.sort((a, b) => {
+            return a.price - b.price
+        });
+        console.log(inventory);
+    }
+
+    function sortSport() {
+        inventory.sort ((a, b) => {
+            return a.refreshRate - b.refreshRate
+    });
+    console.log(inventory);
+}
+
+
   return (
       <main className="page-container">
           <h1>Tech it easy Dashboard </h1>
@@ -31,7 +56,21 @@ function App() {
                     <p className= "best-soldTv-info-box"> {createProductName(bestSellingTv)} </p>
                     <p> {createProductPrice(bestSellingTv)}</p>
                     <p>{createProductScreenSizeString(bestSellingTv.availableSizes)}</p>
-              </section>
+                    <img className="image-of-bestsellingTV" src={createImageTv(bestSellingTv)} alt = "Image bestselling tV"/>
+              <ul className="product-info">
+                  <li><img src={check} alt="Icoon: aanwezig" className="icon"/>wifi</li>
+                  <li><img src={minus} alt="Icoon: niet aanwezig" className="icon"/>speech</li>
+                  <li><img src={check} alt="Icoon: aanwezig" className="icon"/> hdr </li>
+                  <li><img src={check} alt="Icoon: aanweig" className="icon"/>bluetooth</li>
+                  <li><img src={minus} alt="Icoon: niet aanwezig" className="icon"/>ambilight</li>
+              </ul>
+          </section>
+          <section>
+              <h2>Alle Tvs</h2>
+                    <button type="button" onClick={sortBestSellers}> Meest verkocht eerst </button>
+                    <button type="button" onClick={sortCheapest}> Goedkoopste eerst</button>
+                    <button type="button" onClick={sortSport}> Meest geschikt voor sport eerst</button>
+          </section>
       </main>
   )
 }
---
+++ src/helpers/createImageTv.js
@@ -0,0 +1,5 @@
+import {bestSellingTv} from "../constants/inventory.js";
+
+export function createImageTv (imageTV) {
+    return imageTV.sourceImg ;
+}
